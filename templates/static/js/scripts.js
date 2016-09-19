1008525795b4b79654802805357855853da31965
Matt Rothenberg <mattrothenberg@gmail.com>
2016-09-18 21:57:16 -0400

    make smooth scrolling EVEN better by adding scrollspy

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -1,7 +1,7 @@
 var body = document.querySelector('body');
 var hamburger = document.getElementById('menuToggle');
 var sectionNavLinks = document.querySelectorAll('.section-nav li a');
-var componentNav = document.querySelectorAll('.component-nav')[0];
+var componentNav = document.querySelectorAll('.component-nav');
 
 // Override default offset value for ZenScroll plugin.
 zenscroll.setup(null, 100);
@@ -10,14 +10,22 @@ hamburger.addEventListener('click', function() {
 	body.classList.toggle('nav-showing');
 });
 
+gumshoe.init({
+  offset: 100, // Distance in pixels to offset calculations
+});
+
 function fixComponentNavigation() {
+	if( componentNav.length === 0) {
+		return;
+	}
+
 	var doc = document.documentElement;
 	var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);	
 	
 	if (top > 50) {
-		componentNav.classList.add('is-fixed')
+		componentNav[0].classList.add('is-fixed')
 	} else {
-		componentNav.classList.remove('is-fixed')
+		componentNav[0].classList.remove('is-fixed')
 	}
 }
 
