457fb006fcfc0f94310749491442a8461bc94831
Matt Rothenberg <mattrothenberg@gmail.com>
2016-09-18 21:40:55 -0400

    Improve smooth scroll UX

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -1,7 +1,24 @@
 var body = document.querySelector('body');
 var hamburger = document.getElementById('menuToggle');
 var sectionNavLinks = document.querySelectorAll('.section-nav li a');
+var componentNav = document.querySelectorAll('.component-nav')[0];
+
+// Override default offset value for ZenScroll plugin.
+zenscroll.setup(null, 100);
 
 hamburger.addEventListener('click', function() {
 	body.classList.toggle('nav-showing');
 });
+
+function fixComponentNavigation() {
+	var doc = document.documentElement;
+	var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);	
+	
+	if (top > 50) {
+		componentNav.classList.add('is-fixed')
+	} else {
+		componentNav.classList.remove('is-fixed')
+	}
+}
+
+window.addEventListener('scroll', fixComponentNavigation);
